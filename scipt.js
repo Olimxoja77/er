68ca4cf9f57a90403e8bcc1448b60dfa665e1b9a
Olimxoja77 <odilxojaevolimxoja@gmail.com>
2025-02-19 15:28:27 +0300

    Add files via upload

---
+++ scipt.js
@@ -0,0 +1,14 @@
+let h1 = document.querySelector("h1")
+
+
+
+
+ new Typed(h1, {
+    strings:  ['salom'],
+    typeSpeed: 0,
+    backSpeed: 0,
+    backDelay: 500,
+    startDelay: 1000,
+    loop: false,
+
+  });
